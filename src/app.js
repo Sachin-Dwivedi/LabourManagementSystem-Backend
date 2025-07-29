21bcfa5eb3bf735cdecb733b1316a9a0dcca0cd6
Sachin-Dwivedi <sachin.edo@gmail.com>
2025-07-29 23:51:50 +0530

    deploy 4

--- src/app.js
+++ src/app.js
@@ -14,14 +14,16 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-  origin: ['*'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["*"],
+    credentials: true,
+  })
+);
 
 app.use(helmet());
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
@@ -34,6 +36,13 @@ app.use("/api/v1/salaries", salaryRouter);
 app.use("/api/v1/leaves", leaveRouter);
 app.use("/api/v1/performance", performanceRouter);
 app.use("/api/v1/notifications", notificationRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is Healthy",
+  });
+});
 
 // Error Middleware
 app.use(errorMiddleware);
